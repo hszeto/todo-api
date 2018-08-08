a0dc128f2836a8a03bc577bdf710a095ac02642f
Henry Szeto <henryszeto@gmail.com>
2018-08-08 11:42:29 -0700

    factorybot tweaks

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::FunnyName.name }
+    name { Faker::FamousLastWords.last_words }
     completed false
     todo_id nil
   end
--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :todo do
-    title { Faker::FamousLastWords.last_words }
+    title { Faker::Job.title }
     created_by { Faker::Number.number(10) }
   end
 end
