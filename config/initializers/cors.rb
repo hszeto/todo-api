45bc5f41be1a9a21aa44676080248ea5bc9182cc
Henry Szeto <henryszeto@gmail.com>
2022-01-27 01:00:22 -0800

    whitelist cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'todo-react16-app.herokuapp.com'
-    # origins '*'
+    origins 'todo-react16-app.herokuapp.com', 'localhost:3000'
 
     resource '*',
       headers: :any,
