f2dfdc3c010fd7a4b36757a2d6a46e2a579c7a9a
Henry Szeto <henryszeto@gmail.com>
2022-11-07 00:34:25 -0800

    update cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'todo-react16-app.herokuapp.com', 'localhost:3000'
+    origins 'todo-app-p0f6.onrender.com', 'localhost:3000'
 
     resource '*',
       headers: :any,
